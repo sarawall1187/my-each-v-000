23d708b2462fd8247e73873a9bc479f52f6aa58b
Sara Walker <sarawall1187@gmail.com>
2018-12-14 18:25:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+ counter = 0
+  while words.length < counter do |word|
+    puts "#{word}"
+  end
+ end
+  counter += 1
+  words
 end
